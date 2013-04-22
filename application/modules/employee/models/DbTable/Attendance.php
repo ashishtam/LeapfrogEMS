fa6768bed7a0130a75173f5791f9ecc72fde15cb
ashishtam <ashishtamrakar@gmail.com>
2013-04-22 11:30:56 +0545

    files added of employee model

---
+++ application/modules/employee/models/DbTable/Attendance.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Employee_Model_DbTable_Attendance extends Zend_Db_Table_Abstract
+{
+    public $_name = "";
+}
