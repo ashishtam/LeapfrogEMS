322072ddaf924e7c947c32b8b5e619cc5435dd7c
subash <connect.subash@gmail.com>
2013-04-24 08:51:45 +0545

    Layout changed and folders update

--- application/modules/employee/controllers/AttendanceController.php
+++ application/modules/employee/controllers/AttendanceController.php
@@ -14,12 +14,40 @@ class Employee_AttendanceController extends Zend_Controller_Action
     
     public function checkInAction()
     {
+       $objAttendance = new Employee_Model_DbTable_Attendance();
+       
+       $form = new Employee_Form_Attendance();
+       $form->checkIn();
+       
+       $this->view->form = $form;
         
+        if ($this->getRequest()->isPost()) {
+           if ($form->isValid($_POST)) {
+                $data = $form->getValues();
+                
+               $objAttendance->addAttendance($data, '1');
+           }
+         } 
     }
     
     public function checkOutAction()
     {
+        $objAttendance = new Employee_Model_DbTable_Attendance();
         
+        $form = new Employee_Form_Attendance();
+        $form->checkOut();
+        
+        $this->view->form = $form;
+         
+         if ($this->getRequest()->isPost()) {
+            if ($form->isValid($_POST)) {
+                $data = $form->getValues();
+                
+                $objAttendance->updateAttendance($data, '3', '1');
+                
+          
+            }
+         } 
     }
     
     public function applyLeaveAction()
--- application/modules/employee/models/DbTable/Attendance.php
+++ application/modules/employee/models/DbTable/Attendance.php
@@ -8,4 +8,33 @@
 class Employee_Model_DbTable_Attendance extends Zend_Db_Table_Abstract
 {
     public $_name = "Attendance";
+    public function addAttendance($data , $empId)
+    {
+        try
+        {
+            $data['emp_id'] = $empId;
+            $result = $this->insert($data);
+            return $result;
+        }
+        catch(Exception $e)
+        {
+            echo $e->getMessage();
+        }
+    }
+    
+    public function updateAttendance($data, $id, $emp_id)
+    {
+        try
+        {
+            echo "<pre>";
+            print_r($data);
+            echo "</pre>";
+            
+            $this->update($data, 'id='. $id . ' and emp_id ='. $emp_id);
+        }
+        catch(Exception $e)
+        {
+            echo $e->getMessage();
+        }
+    } 
 }
