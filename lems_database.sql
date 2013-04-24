9ae6b7c87ad4c146e0321171273ba7e5131ed575
ashishtam <ashishtamrakar@gmail.com>
2013-04-24 11:49:57 +0545

    database file updated

--- lems_database.sql
+++ lems_database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 23, 2013 at 12:51 PM
+-- Generation Time: Apr 24, 2013 at 11:48 AM
 -- Server version: 5.5.27
 -- PHP Version: 5.4.7
 
@@ -49,7 +49,7 @@ CREATE TABLE IF NOT EXISTS `Attendance` (
 INSERT INTO `Attendance` (`id`, `emp_id`, `date`, `checkin_time`, `checkout_time`, `checkin_status`, `checkout_status`, `checkin_comments`, `checkout_comments`) VALUES
 (1, 1, '2013-04-23', '08:12:05', NULL, 'On Time', NULL, NULL, NULL),
 (2, 1, '0000-00-00', '11:21:00', NULL, 'Late', NULL, 'I was sick', NULL),
-(3, 1, '2013-04-23', '11:32:40', NULL, 'Late', NULL, 'i was on date', NULL);
+(3, 1, '2013-04-23', '11:32:40', '13:06:03', 'Late', 'Early', 'i was on date', 'xsfadklf;jdsa');
 
 -- --------------------------------------------------------
 
@@ -62,10 +62,8 @@ CREATE TABLE IF NOT EXISTS `CV_Info` (
   `emp_id` int(11) NOT NULL,
   `address` varchar(255) NOT NULL,
   `DOB` date NOT NULL,
-  `skills` longtext,
-  `academics` longtext,
-  `achievements` longtext,
-  `training` longtext,
+  `info` longtext NOT NULL,
+  `image_name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `emp_id` (`emp_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
@@ -100,6 +98,7 @@ INSERT INTO `Designation` (`id`, `name`) VALUES
 
 CREATE TABLE IF NOT EXISTS `Employee` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_id` int(11) NOT NULL,
   `full_name` varchar(255) NOT NULL,
   `email_id` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
@@ -107,21 +106,22 @@ CREATE TABLE IF NOT EXISTS `Employee` (
   `designation_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email_id` (`email_id`),
-  KEY `designation_id` (`designation_id`)
+  KEY `designation_id` (`designation_id`),
+  KEY `role_id` (`role_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
 
 --
 -- Dumping data for table `Employee`
 --
 
-INSERT INTO `Employee` (`id`, `full_name`, `email_id`, `password`, `contact`, `designation_id`) VALUES
-(1, 'ashish tamrakar', 'ashishtamrakar@gmail.com', '7b69ad8a8999d4ca7c42b8a729fb0ffd', '9841866792', 1),
-(2, 'ojash dahal', 'ojashdahal@lftechnology.com', 'a5b4c15152e100c43c6dfd97bb45dd3f', '9841023233', 1),
-(3, 'shiv kumar sah', 'shivkumarsha@lftechnology.com', '671fc86500ae5dd534f859e4483354fe', '9841123456', 1),
-(4, 'kiran regmi', 'kiranregmi@lftechnology.com', 'b1a5b64256e27fa5ae76d62b95209ab3', '9841123456', 2),
-(6, 'subash poudyal', 'subashpoudyal@lftechnology.com', 'b4caefa3d450d0e36e183160d17aba24', '9841123456', 1),
-(7, 'Alina Shakya', 'alinashakya@lftechnology.com', '914a23f72f590809d3fe431573ecb71f', '9841123456', 1),
-(8, 'Sudip Pudasaini', 'sudippudasaini@lftechnology.com', '550bbf0991fd493d1afaa2bdd246af6a', '9841123456', 1);
+INSERT INTO `Employee` (`id`, `role_id`, `full_name`, `email_id`, `password`, `contact`, `designation_id`) VALUES
+(1, 1, 'ashish tamrakar', 'ashishtamrakar@gmail.com', '7b69ad8a8999d4ca7c42b8a729fb0ffd', '9841866792', 1),
+(2, 3, 'ojash dahal', 'ojashdahal@lftechnology.com', 'a5b4c15152e100c43c6dfd97bb45dd3f', '9841023233', 1),
+(3, 3, 'shiv kumar sah', 'shivkumarsha@lftechnology.com', '671fc86500ae5dd534f859e4483354fe', '9841123456', 1),
+(4, 1, 'kiran regmi', 'kiranregmi@lftechnology.com', 'b1a5b64256e27fa5ae76d62b95209ab3', '9841123456', 2),
+(6, 1, 'subash poudyal', 'subashpoudyal@lftechnology.com', 'b4caefa3d450d0e36e183160d17aba24', '9841123456', 1),
+(7, 1, 'Alina Shakya', 'alinashakya@lftechnology.com', '914a23f72f590809d3fe431573ecb71f', '9841123456', 1),
+(8, 1, 'Sudip Pudasaini', 'sudippudasaini@lftechnology.com', '550bbf0991fd493d1afaa2bdd246af6a', '9841123456', 1);
 
 -- --------------------------------------------------------
 
@@ -132,7 +132,8 @@ INSERT INTO `Employee` (`id`, `full_name`, `email_id`, `password`, `contact`, `d
 CREATE TABLE IF NOT EXISTS `Leave` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `emp_id` int(11) NOT NULL,
-  `leave_date` date NOT NULL,
+  `from_date` date NOT NULL,
+  `to_date` date NOT NULL,
   `reason` longtext NOT NULL,
   PRIMARY KEY (`id`),
   KEY `emp_id` (`emp_id`)
@@ -165,7 +166,19 @@ CREATE TABLE IF NOT EXISTS `Resources` (
   `name` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `Resources`
+--
+
+INSERT INTO `Resources` (`id`, `name`, `description`) VALUES
+(1, 'admin_resources', 'Resources Controller - Admin Module'),
+(2, 'admin_index', 'Index Controller - Admin Module'),
+(3, 'admin_permission', 'Permission Controller - Admin Module'),
+(4, 'employee_index', 'Index Controller - Employee Module'),
+(5, 'employee_attendance', 'Attendance Controller - Employee Module'),
+(6, 'front_index', 'Index Controller - Front Module');
 
 -- --------------------------------------------------------
 
@@ -175,11 +188,18 @@ CREATE TABLE IF NOT EXISTS `Resources` (
 
 CREATE TABLE IF NOT EXISTS `Roles` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `emp_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `emp_id` (`emp_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `Roles`
+--
+
+INSERT INTO `Roles` (`id`, `name`) VALUES
+(1, 'Super Admin'),
+(2, 'Admin'),
+(3, 'Employee');
 
 --
 -- Constraints for dumped tables
@@ -201,7 +221,8 @@ ALTER TABLE `CV_Info`
 -- Constraints for table `Employee`
 --
 ALTER TABLE `Employee`
-  ADD CONSTRAINT `Employee_ibfk_1` FOREIGN KEY (`designation_id`) REFERENCES `Designation` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `Employee_ibfk_1` FOREIGN KEY (`designation_id`) REFERENCES `Designation` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `Employee_ibfk_4` FOREIGN KEY (`role_id`) REFERENCES `Roles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `Leave`
@@ -215,12 +236,6 @@ ALTER TABLE `Leave`
 ALTER TABLE `Permissions`
   ADD CONSTRAINT `Permissions_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `Roles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `Permissions_ibfk_2` FOREIGN KEY (`resource_id`) REFERENCES `Resources` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `Roles`
---
-ALTER TABLE `Roles`
-  ADD CONSTRAINT `Roles_ibfk_1` FOREIGN KEY (`emp_id`) REFERENCES `Employee` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
